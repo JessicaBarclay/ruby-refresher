650140b3abb3fadb786d81009f29c41fa3b4dbc5
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-27 00:25:48 +0100

    Passes: average_of_array(array)

--- lib/questions.rb
+++ lib/questions.rb
@@ -110,6 +110,8 @@ end
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  array = array.inject { |sum, i| sum + i }.to_f / array.length
+  array.ceil
 end
 
 # get all the elements in an array, up until the first element
