2b1e30adeec8fb834b4c84bffb6d331b182cc319
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-27 00:16:52 +0100

    Passes: double_array(array)

--- lib/questions.rb
+++ lib/questions.rb
@@ -99,6 +99,8 @@ end
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  clone = array.clone
+  clone + array
 end
 
 # convert a symbol into a string
