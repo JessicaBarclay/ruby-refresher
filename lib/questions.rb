5d538447566cecb5b7c3d3b505877d8bf17e3bc4
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-27 18:03:19 +0100

    Passes: get_first_half_of_string(string)
    Help from pmuldoon86(Github)

--- lib/questions.rb
+++ lib/questions.rb
@@ -53,6 +53,8 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  length_divided_by_2 = (string.length.to_f / 2).ceil
+  string[0..(length_divided_by_2 - 1)]
 end
 
 # turn a positive integer into a negative integer. A negative integer
